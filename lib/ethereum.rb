3b50d69f0cc550b7ea914e55da37285fe935d2f8
shideneyu <shideneyu@gmail.com>
2021-06-14 23:46:47 +0200

    Fefactoring: Require Relative

--- lib/ethereum.rb
+++ lib/ethereum.rb
@@ -5,25 +5,11 @@ require 'digest/sha3'
 require 'pry'
 
 module Ethereum
-  require_relative 'ethereum/abi'
-  require_relative 'ethereum/client'
-  require_relative 'ethereum/ipc_client'
-  require_relative 'ethereum/http_client'
-  require_relative 'ethereum/singleton'
-  require_relative 'ethereum/solidity'
-  require_relative 'ethereum/initializer'
-  require_relative 'ethereum/contract'
-  require_relative 'ethereum/explorer_url_helper'
-  require_relative 'ethereum/function'
-  require_relative 'ethereum/function_input'
-  require_relative 'ethereum/function_output'
-  require_relative 'ethereum/contract_event'
-  require_relative 'ethereum/encoder'
-  require_relative 'ethereum/decoder'
-  require_relative 'ethereum/formatter'
-  require_relative 'ethereum/transaction'
-  require_relative 'ethereum/deployment'
-  require_relative 'ethereum/project_initializer'
-  require_relative 'ethereum/contract_initializer'
+  ruby_project_files = Dir[File.join(File.dirname(__FILE__), '**', '*.rb')]
+
+  ruby_project_files.sort_by!{ |file_name| file_name.downcase }.each do |path|
+    require_relative path unless path.match?(/rail/)
+  end
+
   require_relative 'ethereum/railtie' if defined?(Rails)
 end
