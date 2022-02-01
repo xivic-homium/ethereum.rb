da099c1f4aa6a571620f16cd6c8a087337d718a5
Cristi Stefan <cristian.stefan@xivic.com>
2022-02-01 16:55:57 +0200

    hardcode gas price

--- lib/ethereum/client.rb
+++ lib/ethereum/client.rb
@@ -122,8 +122,8 @@ module Ethereum
         args << "latest"
       end
 
-      args[:gas] = '0x0'
-      args[:gasPrice] = '0x0'
+      # args[:gas] = '0x0'
+      # args[:gasPrice] = '0x0'
 
       payload = {jsonrpc: "2.0", method: command, params: encode_params(args), id: get_id}
       @logger.info("Sending #{payload.to_json}") if @log
